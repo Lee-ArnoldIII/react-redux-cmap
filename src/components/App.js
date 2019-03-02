33b23ba1f77ef0b3aacd2dfbaf0c3a18180d3bbc
Lee Arnold <lee.arnoldiii@gmail.com>
2019-03-02 13:20:41 -0600

    updated structure of files. added component files, containers, reducers, and test files. readme will be updated at a later time.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        The app is up and running!
+      </div>
+    )
+  }
+}
+
+export default App
