e1d5a67fe05e59e3b2c2e5b3a03f2f2fafe2708d
Audy Arandela <audyarandela@gmail.com>
2019-03-03 12:07:19 -0600

    Added Redux: Provider and store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Components
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Redux 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers/reducer'
+
+const store = createStore(reducers)
+
+ReactDOM.render(<Provider store={store}>
+                  <App />
+                </Provider><App />, document.getElementById('root'))
