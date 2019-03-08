fe13f69e4eead92d5fefcf8d284e412db3919acb
Lee Arnold <lee.arnoldiii@gmail.com>
2019-03-08 13:36:59 -0600

    updated requests to add functions and handle controls

--- src/components/student/Requests.js
+++ src/components/student/Requests.js
@@ -1,20 +1,46 @@
 import React from 'react'
+import { Connect } from 'react-redux'
 
-const Requests = () => {
-  return (
-    <div className=''>
-      <div className='box'>
-        <h1 className='title has-text-centered'>Assistance/Restroom Request Form</h1>
+class Requests extends Component {
+  constructor (props) {
+    super(props)
 
-        <div className='field'>
-          <label className='label'>Question</label>
-          <div className='control'>
-            <input className='input' type='text' placeholder='What is your question?' />
+    this.state = {
+      requestQuestion: '',
+      rrRequest: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (evt) {
+    console.log('evt.target.value')
+    this.setState(evt.target.value)
+  }
+
+  handleSubmit (evt) {
+    console.log('Request submitted')
+    const { index } = this.props
+    evt.preventDefault()
+    this.props.submit(this.state.requestQuestion, index)
+  }
+
+  render () {
+    return (
+      <div className=''>
+        <div className='box'>
+          <h1 className='title has-text-centered'>Assistance/Restroom Request Form</h1>
+
+          <div className='field'>
+            <label className='label'>Question</label>
+            <div className='control'>
+              <input className='input' type='text' placeholder='What is your question?' />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Requests
