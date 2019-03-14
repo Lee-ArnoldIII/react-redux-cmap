aad30579de190f0e954def83698e2b999af5888f
Audy Arandela <audyarandela@gmail.com>
2019-03-14 16:54:20 -0500

    refactored some more and adding date

---
+++ src/components/teacher/Date.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Date extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     date: new Date()
+  //   }
+    
+  // }
+
+  // componentDidMount() {
+  //   this.timerID = setInterval(
+  //     () => this.tick(),
+  //     1000
+  //   )
+  // }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.timerID);
+  // }
+
+  // tick() {
+  //   this.setState({
+  //     date: new Date()
+  //   });
+  // }
+
+  render() {
+    
+    return (
+      <div className='section'>
+        <h2>It is .</h2>
+      </div>
+    )
+  }
+}
--- src/components/teacher/TeacherDashboard.js
+++ src/components/teacher/TeacherDashboard.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import RosterTable from './RosterTable'
 import AgendaFormContainer from '../../containers/AgendaFormContainer'
+import Date from './Date'
 
 class TeacherDashboard extends Component {
   render () {
     return (
+      <React.Fragment>
+        <Date />
       <div className='columns' id='teacher'>
+        
         <RosterTable />
         <AgendaFormContainer />
       </div>
+      </React.Fragment>
     )
   }
 }
